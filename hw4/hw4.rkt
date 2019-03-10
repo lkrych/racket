a60d26fadbbbae9442f28fafd1007e67a9825bc5
Leland Krych <Leland.Krych@gmail.com>
2019-03-09 22:54:54 -0800

    Add sequence function

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -5,3 +5,11 @@
 
 ;; put your code below
 
+;; sequence takes 3 arguments, low, high, and stride and produces a list from low to high,
+;; including low and possibly high, separated by stride and in sorted order
+
+(define (sequence low high stride)
+  (if ( > low high)
+      null
+      (cons low (sequence (+ low stride) high stride))))
+
